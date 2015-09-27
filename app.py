c85f58208f84a97e7bea7327f8af6ae0f3a83d9d
Dayoung Lee <pescadlee@gmail.com>
2015-09-27 10:10:13 -0500

    app.py updated

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, request, send_from_directory
-import json, random, os
+import json, random, os, re
 
 app = Flask(__name__)
 
 with open('static/phrases.txt') as f:
     phrases = f.readlines()
 
+ignore = ['i', 'a', 'an', 'the']
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
+		array = random.choice(phrases).split(" ")
+		while(True):
+			temp = random.choice(array)
+			if temp in ignore:
+				continue
+			else:
+				x = 0
+				for y in range(0, len(array)):
+					if array[y] == temp:
+						x = y;
+						break;
+				break
+			
+			#array[x] would be changed
+				
 		return render_template('index.html')
 	elif request.method == 'POST':
 		return render_template('index.html', genpun=random.choice(phrases))
