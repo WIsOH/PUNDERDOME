977d917bf05f27bf114cd97ef5085358f74c5c63
Dayoung Lee <pescadlee@gmail.com>
2015-09-27 06:45:55 -0500

    gotta commit

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-	if request.method == 'POST':
-		return render_template('outpun.html')
-	else:
-		return render_template('index.html')
+	if request.method == 'POST'
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+# debugging
+@app.route('/hello')
+def hello():
+	return render_template('hello.html')
+	
 
 if __name__ == "__main__":
 	app.debug = True
